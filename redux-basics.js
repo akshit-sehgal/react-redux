1cbacde19284c420a923ef7b4b7449cc090ea7b4
Akshit Sehgal <akshitsehgal95@gmail.com>
2018-07-08 22:27:39 +0530

    Dispatching Actions

--- redux-basics.js
+++ redux-basics.js
@@ -7,6 +7,18 @@ const initialState ={
 
 // Reducer
 const rootReducer = (prevState = initialState,action) => {
+    switch(action.type){
+        case 'INC_COUNTER':
+            return{
+                ...prevState,
+                counter: prevState.counter + 1
+            }
+        case 'ADD_COUNTER':
+            return {
+                ...prevState,
+                counter: prevState.counter + action.value
+            }
+    }
     return prevState;
 };
 
@@ -15,3 +27,12 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({
+    type: 'INC_COUNTER'
+});
+store.dispatch({
+    type: 'ADD_COUNTER',
+    value: 10
+});
+
+console.log(store.getState());
