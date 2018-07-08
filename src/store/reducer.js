016a264675a66918df17846fa0c980085d99b6e8
Akshit Sehgal <akshitsehgal95@gmail.com>
2018-07-08 22:53:00 +0530

    Dispatching actions from within components

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,9 @@ const initialState = {
     counter: 0
 }
 const reducer = (prevState = initialState, action)=>{
+    switch(action.type){
+        case 'INCREMENT': return {counter:prevState.counter+1};
+    }
     return prevState;
 };
 export default reducer;
